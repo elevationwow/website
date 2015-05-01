f716810029dc38a055ddd642465f64f834f04399
Sean Anderson <wing5wong@hotmail.com>
2015-05-02 01:15:14 +1200

    add progression base
    begin work on progression widget

--- resources/views/partials/recruitment/widget.blade.php
+++ resources/views/partials/recruitment/widget.blade.php
@@ -4,13 +4,13 @@
         <div class="recruitment__body">
 
             <div class="recruitment__body__class class--dk">
-                <div class="class_spec class_spec--dk dk--blood">Blood</div>
+                <div class="class_spec class_spec--dk dk--blood class_spec--not-recruiting">Blood</div>
                 <div class="class_spec class_spec--dk dk--frost">Frost</div>
-                <div class="class_spec class_spec--dk dk--unholy">Unholy</div>
+                <div class="class_spec class_spec--dk dk--unholy class_spec--not-recruiting">Unholy</div>
             </div>
 
             <div class="recruitment__body__class class--druid">
-                <div class="class_spec class_spec--druid druid--balance">Balance</div>
+                <div class="class_spec class_spec--druid druid--balance class_spec--not-recruiting">Balance</div>
                 <div class="class_spec class_spec--druid druid--feral">Feral</div>
                 <div class="class_spec class_spec--druid druid--guardian">Guardian</div>
                 <div class="class_spec class_spec--druid druid--resto">Resto</div>
@@ -18,56 +18,56 @@
 
             <div class="recruitment__body__class class--hunter">
                 <div class="class_spec class_spec--hunter hunter--surv">Survival</div>
-                <div class="class_spec class_spec--hunter hunter--mm">Marksman</div>
+                <div class="class_spec class_spec--hunter hunter--mm class_spec--not-recruiting">Marksman</div>
                 <div class="class_spec class_spec--hunter hunter--bm">Beast Mastery</div>
             </div>
 
             <div class="recruitment__body__class class--mage">
-                <div class="class_spec class_spec--mage mage--arcane">Ar</div>
-                <div class="class_spec class_spec--mage mage--fire">Fi</div>
-                <div class="class_spec class_spec--mage mage--frost">Fr</div>
+                <div class="class_spec class_spec--mage mage--arcane class_spec--not-recruiting">Arcane</div>
+                <div class="class_spec class_spec--mage mage--fire class_spec--not-recruiting">Fire</div>
+                <div class="class_spec class_spec--mage mage--frost class_spec--not-recruiting">Frost</div>
             </div>
 
             <div class="recruitment__body__class class--monk">
-                <div class="class_spec class_spec--monk monk--mw">MW</div>
-                <div class="class_spec class_spec--monk monk--bm">BM</div>
-                <div class="class_spec class_spec--monk monk--ww">WW</div>
+                <div class="class_spec class_spec--monk monk--mw">Mistweaver</div>
+                <div class="class_spec class_spec--monk monk--bm">Beast Mastery</div>
+                <div class="class_spec class_spec--monk monk--ww">Windwalker</div>
             </div>
 
             <div class="recruitment__body__class class--rogue">
-                <div class="class_spec class_spec--rogue rogue--sub">Su</div>
-                <div class="class_spec class_spec--rogue rogue--combat">Co</div>
-                <div class="class_spec class_spec--rogue rogue--ass">As</div>
+                <div class="class_spec class_spec--rogue rogue--sub">Subtlety</div>
+                <div class="class_spec class_spec--rogue rogue--combat">Combat</div>
+                <div class="class_spec class_spec--rogue rogue--ass class_spec--not-recruiting">Assassination</div>
             </div>
 
             <div class="recruitment__body__class class--priest">
-                <div class="class_spec class_spec--priest priest--shadow">Sh</div>
-                <div class="class_spec class_spec--priest priest--disc">Di</div>
-                <div class="class_spec class_spec--priest priest--holy">Ho</div>
+                <div class="class_spec class_spec--priest priest--shadow class_spec--not-recruiting">Shadow</div>
+                <div class="class_spec class_spec--priest priest--disc class_spec--not-recruiting">Discipline</div>
+                <div class="class_spec class_spec--priest priest--holy">Holy</div>
             </div>
 
             <div class="recruitment__body__class class--paladin">
-            <div class="class_spec class_spec--paladin paladin--ret">Re</div>
-                <div class="class_spec class_spec--paladin paladin--prot">Pr</div>
-                <div class="class_spec class_spec--paladin paladin--holy">Ho</div>
+            <div class="class_spec class_spec--paladin paladin--ret">Retribution</div>
+                <div class="class_spec class_spec--paladin paladin--prot">Protection</div>
+                <div class="class_spec class_spec--paladin paladin--holy">Holy</div>
             </div>
 
             <div class="recruitment__body__class class--shaman">
-            <div class="class_spec class_spec--shaman shaman--ele">El</div>
-                <div class="class_spec class_spec--shaman shaman--enhance">En</div>
-                <div class="class_spec class_spec--shaman shaman--resto">Re</div>
+            <div class="class_spec class_spec--shaman shaman--ele class_spec--not-recruiting">Elemental</div>
+                <div class="class_spec class_spec--shaman shaman--enhance">Enhance</div>
+                <div class="class_spec class_spec--shaman shaman--resto">Restoration</div>
             </div>
 
             <div class="recruitment__body__class class--warlock">
-            <div class="class_spec class_spec--warlock warlock--destro">Des</div>
-                <div class="class_spec class_spec--warlock warlock--aff">Af</div>
-                <div class="class_spec class_spec--warlock warlock--demo">Dem</div>
+            <div class="class_spec class_spec--warlock warlock--destro">Destruction</div>
+                <div class="class_spec class_spec--warlock warlock--aff">Affliction</div>
+                <div class="class_spec class_spec--warlock warlock--demo">Demonology</div>
             </div>
 
             <div class="recruitment__body__class class--warrior">
-            <div class="class_spec class_spec--warrior warrior--prot">Pr</div>
-                <div class="class_spec class_spec--warrior warrior--fury">Fu</div>
-                <div class="class_spec class_spec--warrior warrior--arms">Ar</div>
+            <div class="class_spec class_spec--warrior warrior--prot">Protection</div>
+                <div class="class_spec class_spec--warrior warrior--fury">Fury</div>
+                <div class="class_spec class_spec--warrior warrior--arms">Arms</div>
             </div>
 
         </div>
